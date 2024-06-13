116f6ed9153020e29b3eed1115cb182d95640274
GiorgiMachablishvili <g.machablishvili@yahoo.com>
2024-06-13 14:27:30 +0400

    fix collection view

--- Practical 1/Scenes/Collection View Controller/CollectionviewController.swift
+++ Practical 1/Scenes/Collection View Controller/CollectionviewController.swift
@@ -8,21 +8,24 @@
 import UIKit
 import SnapKit
 
-class CollectionViewController: UIViewController  {
+class CollectionViewController: UIViewController {
     private lazy var collectionView: UICollectionView = {
         let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .vertical
+        layout.minimumLineSpacing = 10
+        layout.minimumInteritemSpacing = 10
         let view = UICollectionView(frame: .zero, collectionViewLayout: layout)
         view.dataSource = self
         view.delegate = self
         view.backgroundColor = .white
-        view.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "cell")
+        view.register(UserCell.self, forCellWithReuseIdentifier: "UserCell")
         return view
     }()
-
     
+    let personInfo = UserProfile.sampleData()
+
     override func viewDidLoad() {
         super.viewDidLoad()
-
         setup()
         setupConstraints()
     }
@@ -40,18 +43,25 @@ class CollectionViewController: UIViewController  {
 
 extension CollectionViewController: UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 10
+        return personInfo.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let width = (collectionView.frame.size.width) / 2
+        return CGSize(width: width, height: width)
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
-        cell.backgroundColor = .red
-        return cell
-    }
+            guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "UserCell", for: indexPath) as? UserCell else {
+                fatalError("Unable to dequeue UserProfileCell")
+            }
+            let profile = personInfo[indexPath.item]
+            cell.configure(with: profile)
+            return cell
+        }
     
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         let selectVc = CollectionViewController()
         navigationController?.present(selectVc, animated: false)
     }
 }
-
